4490890f202e268b6385f3a7efe73cc7b4c33c26
4nc3str4l <cmuri_10@hotmail.com>
2023-09-09 00:01:39 +0200

    UI script + LoadSceneByName

---
+++ UI/CanvasGroupController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.UI
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupController : MonoBehaviour
+    {
+        private List<string> _openers = new List<string>();
+        public bool Visible => CanvasGroup.alpha > 0f;
+        
+        public CanvasGroup CanvasGroup {
+            get
+            {
+                if (_canvasGroup == null)
+                {
+                    _canvasGroup = GetComponent<CanvasGroup>();
+                }
+                return _canvasGroup;
+            }
+        }
+    
+        private CanvasGroup _canvasGroup;
+
+        public virtual void Show()
+        {
+            OnShow();
+        }
+
+        public virtual void Hide()
+        {
+            OnHide();
+        }
+
+        public void SetVisible(bool visible)
+        {
+            if(visible)
+            {
+                OnShow();
+            }
+            else
+            {
+                OnHide();
+            }
+        }
+
+        private void OnShow()
+        {
+            CanvasGroup.alpha = 1f;
+            CanvasGroup.interactable = true;
+            CanvasGroup.blocksRaycasts = true;
+        }
+
+        private void OnHide()
+        {
+            CanvasGroup.alpha = 0f;
+            CanvasGroup.interactable = false;
+            CanvasGroup.blocksRaycasts = false;
+        }
+        
+    }
+}
---
+++ Unity/LoadSceneByName.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utils.Unity
+{
+    public class LoadSceneByName : MonoBehaviour
+    {
+        public string sceneToLoad;
+        
+        public void LoadScene()
+        {
+            if (string.IsNullOrEmpty(sceneToLoad))
+            {
+                Debug.LogError("Scene to load is not set");
+                return;
+            }
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
