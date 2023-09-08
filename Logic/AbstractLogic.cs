4dbbeb3087c1750ba0975891abc8c3df7661fada
4nc3str4l <cmuri_10@hotmail.com>
2023-09-08 10:16:42 +0200

    Explanation for AbstractLogic and OndestroyObservable

--- Logic/AbstractLogic.cs
+++ Logic/AbstractLogic.cs
@@ -1,6 +1,13 @@
 ﻿namespace Utils.Logic
 {
 
+    /// <summary>
+    /// I find myself always writing this pattern where I need to create
+    /// some sort of script that will need to be setup, disposed and updated
+    /// so I just included it here.
+    /// This is useful for state machines, or any other logic that needs to
+    /// follow this pattern.
+    /// </summary>
     public abstract class AbstractLogic
     {
         protected bool IsSetup;
--- Observables/OnDestroyObservable.cs
+++ Observables/OnDestroyObservable.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Utils
+namespace Utils.Observables
 {
+    /// <summary>
+    /// This class is used to observe the OnDestroy event of a MonoBehaviour without having to
+    /// do the annoying thing for having an event and then usubscribe at destroy time.
+    /// </summary>
     public class OnDestroyObservable : MonoBehaviour
     {
         
