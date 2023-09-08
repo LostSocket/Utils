d9269914d51eae5ae8b1bb40401555e0534c692e
4nc3str4l <cmuri_10@hotmail.com>
2023-09-08 09:56:25 +0200

    Destroy Observable

---
+++ OnDestroyObservable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public class OnDestroyObservable : MonoBehaviour
+    {
+        
+        private readonly List<Action> _onDestroyActions = new();
+        
+        public void AddOnDestroyAction(Action action)
+        {
+            _onDestroyActions.Add(action);
+        }
+        
+        public void RemoveOnDestroyAction(Action action)
+        {
+            _onDestroyActions.Remove(action);
+        }
+        
+        private void OnDestroy()
+        {
+            foreach (var action in _onDestroyActions)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
