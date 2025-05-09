fc1ee7715fa2e7b97b36b3757430e9226f3c5329
4nc3str4l <cmuri_10@hotmail.com>
2025-05-09 08:01:50 +0200

    Make the interactive object able to be outlined for each renderer.

--- Interaction/InteractiveObject.cs
+++ Interaction/InteractiveObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Utils.Interaction
@@ -7,18 +8,75 @@ namespace Assets.Scripts.Utils.Interaction
     {
         public string Name = "Interactive Object";
         public string InteractionDescription = "Pickup";
-
-        public event Action<InteractiveObject> OnDestroyed;
         
-        public abstract void OnInteract();
-        public abstract void OnFocused();
-        public abstract void OnUnfocused();
+        private Dictionary<MeshRenderer, Material[]> m_UnfocusedMaterials;
+        private Dictionary<MeshRenderer, Material[]> m_FocusedMaterials;
 
-        public void SafeDestroy()
+        private MeshRenderer[] m_MeshRenderers;
+        private bool m_IsInitialized;
+        
+        public event Action<InteractiveObject> OnDestroyed;
+
+        public void Interact()
+        {
+            OnInteract();
+        }
+
+        public void Focused(Material _focusedMaterial)
+        {
+            if (!m_IsInitialized)
+            {
+                InitializeMaterials(_focusedMaterial);
+                m_IsInitialized = true;
+            }
+
+            foreach (var r in m_MeshRenderers)
+            {
+                r.materials = m_FocusedMaterials[r];
+            }
+            OnFocused();
+        }
+
+        public void UnFocused()
+        {
+            OnUnfocused();
+            if (m_MeshRenderers == null) return;
+
+            foreach (var r in m_MeshRenderers)
+            {
+                r.materials = m_UnfocusedMaterials[r];
+            }
+        }
+        
+        private void InitializeMaterials(Material _focusedMaterial)
+        {
+            m_MeshRenderers = GetComponentsInChildren<MeshRenderer>(true);
+            m_UnfocusedMaterials = new Dictionary<MeshRenderer, Material[]>();
+            m_FocusedMaterials = new Dictionary<MeshRenderer, Material[]>();
+
+            foreach (var r in m_MeshRenderers)
+            {
+                var unfocused_materials = r.materials;
+                m_UnfocusedMaterials.Add(r, unfocused_materials);
+
+                var focused_materials = new Material[unfocused_materials.Length + 1];
+                Array.Copy(unfocused_materials, focused_materials, unfocused_materials.Length);
+
+                var focused_material_instance = Instantiate(_focusedMaterial);
+                focused_materials[unfocused_materials.Length] = focused_material_instance;
+
+                m_FocusedMaterials.Add(r, focused_materials);
+            }
+        }
+
+        protected abstract void OnInteract();
+        protected abstract void OnFocused();
+        protected abstract void OnUnfocused();
+
+        protected void SafeDestroy()
         {
             OnDestroyed?.Invoke(this);
             Destroy(gameObject);
         }
     }
-    
 }
